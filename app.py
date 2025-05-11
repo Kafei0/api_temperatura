b08ace936b6967bb880f39179d6fe3155051e981
Kafei0 <josmarth09@gmail.com>
2025-05-11 08:29:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 datos_actuales = {}
 
 @app.route('/')
 def home():
-    return "API de temperatura funcionando"
+    return "API de temperatura funcionando 🚀"
 
 @app.route('/temperatura', methods=['POST'])
 def recibir_temperatura():
@@ -22,3 +23,8 @@ def recibir_temperatura():
 @app.route('/temperatura', methods=['GET'])
 def obtener_temperatura():
     return jsonify(datos_actuales), 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
